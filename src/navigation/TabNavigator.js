40bc397de48d5d80bff798be7fc5191b27ee2ea4
Pedro070703 <127454552+Pedro070703@users.noreply.github.com>
2023-09-14 11:38:52 -0300

    TabNavigator.js

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
+import { View } from "react-native";
 import Home from "../components/Home";
 import LoginForm from "../components/LoginForm";
 import RandomCard from "../components/Pokerandom";
@@ -8,11 +9,67 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Login" component={LoginForm} />
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Pokemon Aleatorio" component={RandomCard} />
-      
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: "#003366",
+        inactiveTintColor: "gray",
+        labelStyle: {
+          fontSize: 14,
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Login"
+        component={LoginForm}
+        options={{
+          tabBarLabel: "Login",
+          tabBarIcon: ({ color, size }) => (
+            <View
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: color, 
+                borderRadius: 15, 
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <View
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: color,
+                borderRadius: 15,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokemon Aleatorio"
+        component={RandomCard}
+        options={{
+          tabBarLabel: "Pokemon Aleatório",
+          tabBarIcon: ({ color, size }) => (
+            <View
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: color,
+                borderRadius: 15,
+              }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
